1cb75ec28ac05408897024fcfdf456d983b77b56
Vinh Nguyen <quangvinh.1305@gmail.com>
2016-07-07 00:36:26 +0700

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #   
-User.delete_all!
+
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
