9b6bd1b277f8316a57f55b16b7dc7994b4b444f4
Vinh Nguyen <quangvinh.1305@gmail.com>
2016-07-06 14:53:44 +0700

    Email user

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,10 +28,12 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
+  
     # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
