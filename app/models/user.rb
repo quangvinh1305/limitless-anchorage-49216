c660a6fa3d5f8d072733126783e0fb40e36300a5
Vinh Nguyen <quangvinh.1305@gmail.com>
2016-07-06 15:23:50 +0700

    kkjdjksd with '#' will be ignored, and an empty message aborts the commit.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
+                    format: { with: VALID_EMAIL_REGEX }
                     # uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
-  
+
     # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
