ccb1c336d8dd27a14518a6d1a925ded913cfd6dc
Quang Vinh Nguyen <vinhnq@zigexn.vn>
2016-07-14 16:23:01 +0700

    fix order permission

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
-  before_action :admin_authentication, only: [:admin, :destroy, :edit]
+  before_action :admin_authentication, only: [:admin, :destroy, :edit, :update]
 
   # GET /orders
   # GET /orders.json
